352602b01c28ba38ed0b4494b7abce381c3b1daa
ikeda <ikd618@gmail.com>
2018-07-08 11:23:43 +0900

    Add post hashtag

--- App/src/scenes/Start.js
+++ App/src/scenes/Start.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
 import { Actions } from 'react-native-router-flux';
-import { Button, Text } from 'native-base';
+import * as firebase from 'firebase';
+import { Content, Input, Item, Button, Text } from 'native-base';
 import styled from 'styled-components';
 import { Layout } from '../components/';
 
 export default class Start extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      hashtag: ''
+    };
+  }
+
+  submitText = () => {
+    firebase.database().ref("/hashtags").set({text: this.state.hashtag});
+    Actions.record();
+  }
+
   render() {
+    const { hashtag } = this.state;
     return (
       <Layout>
-        <StyledButton
-          full
-          onPress={() => Actions.record()}
-        >
-          <Text>Start</Text>
-        </StyledButton>
+        <Content>
+          <Text>旅にタグはつきものです</Text>
+          <Item regular>
+            <Input
+              value={hashtag}
+              placeholder='Regular Textbox'
+              onChangeText={(text) => this.setState({hashtag: text})}
+            />
+          </Item>
+          <StyledButton
+            full
+            onPress={() => this.submitText()}
+          >
+            <Text>Start</Text>
+          </StyledButton>
+        </Content>
       </Layout>
     )
   }
