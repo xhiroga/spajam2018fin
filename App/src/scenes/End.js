4378e4c1579bf3d19797154fc786bc794016695e
Rentearo Igari <>
2018-07-08 12:17:25 +0900

    現状

--- App/src/scenes/End.js
+++ App/src/scenes/End.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
-import { Button, Text } from 'native-base';
+import { Image } from 'react-native';
+import { Button, Text, View } from 'native-base';
 import styled from 'styled-components';
 import { Layout } from '../components/';
 
 export default class End extends Component {
   render() {
     return (
-      <Layout>
+      <BackGround>
+        <BGImage
+          source={require('../../img/bg.png')}
+        />
         <StyledButton
           full
         >
-          <Text>Start</Text>
+          <Text>ホームに戻る</Text>
         </StyledButton>
-      </Layout>
+      </BackGround>
     )
   }
 }
 
+const BackGround = styled(View)`
+  backgroundColor: rgb(189, 231, 240);
+  flex: 1;
+`
+
+const BGImage = styled(Image)`
+  margin: 30px auto 20px;
+  width: 280px;
+  height: 500px;
+`
+
 const StyledButton = styled(Button)`
   margin: 15px 10px;
+  backgroundColor: rgb(240, 43, 96);
+  border-radius: 10;
 `
